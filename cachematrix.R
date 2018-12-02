31b996a64d3ce82929c1edd561919404de0fc857
trailblaiser <ftmblaisecruz@gmail.com>
2018-12-02 10:39:24 +0800

    First commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This provides a less time-consuming way to solve the inverse of a matrix
+## The following functions will cache the inverse of the matrix
+## Matrices supplied here are assumed to be invertible
 
-## Write a short comment describing this function
+## creates a special "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  set_inv <- function(inverse) inv <<- inverse
+  get_inv <- function() inv
+  list(set = set, get = get,
+       set_inv = set_inv,
+       get_inv = get_inv)
 }
 
 
-## Write a short comment describing this function
+## computes the inverse of the special "matrix" returned by makeCacheMatrix above
+## If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$get_inv()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data)
+  x$set_inv(inv)
+  inv
 }
+
+## TESTING THE PROGRAM:
+## (matrix example source: http://www.mathcentre.ac.uk/resources/uploaded/sigma-matrices7-2009-1.pdf)
+## am <- matrix(c(4, 3, 1, 1), 2, 2, byrow = TRUE)
+## am
+##      [,1] [,2]
+## [1,]    4    3
+## [2,]    1    1
+## am1 <- makeCacheMatrix(am)
+## cacheSolve(am1)
+##      [,1] [,2]
+## [1,]    1   -3
+## [2,]   -1    4
+
